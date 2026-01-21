637b93714b47cd2389f52bd3741c744ea6da3dad
mfris26 <farisalifiansyah95@gmail.com>
2026-01-21 18:41:44 +0700

    Add files via upload

---
+++ TugasC#/percabangan.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int usia = 20;
+
+        if (usia >= 18)
+        {
+            Console.WriteLine("Status: Dewasa");
+        }
+        else
+        {
+            Console.WriteLine("Status: Di bawah umur");
+        }
+    }
+}
